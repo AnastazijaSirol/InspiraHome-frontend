abab2ca9c251dcc33a449f251704d92d33f9e04e
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-11-03 21:15:29 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/Home.vue'
 import AboutPage from './components/About.vue'
+import ProfilePage from './components/Profile.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/about', component: AboutPage }
+  { path: '/about', component: AboutPage },
+  { path: '/profile', component: ProfilePage}
 ]
 
 const router = createRouter({
