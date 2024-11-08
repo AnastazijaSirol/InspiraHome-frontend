7ad6b9da39aac5d6750854167cfaacb73e0c6866
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-11-09 00:00:01 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/Home.vue'
 import AboutPage from './components/About.vue'
 import ProfilePage from './components/Profile.vue'
+import GroupPage from './components/GroupChats.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/about', component: AboutPage },
-  { path: '/profile', component: ProfilePage}
+  { path: '/profile', component: ProfilePage},
+  { path: '/group-chats', component: GroupPage}
 ]
 
 const router = createRouter({
