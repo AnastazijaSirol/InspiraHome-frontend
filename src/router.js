3f5e01f900abe0673cd915716cad3e094d75246d
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-11-21 15:44:49 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,13 +4,15 @@ import AboutPage from './components/About.vue'
 import ProfilePage from './components/Profile.vue'
 import GroupPage from './components/GroupChats.vue'
 import DesignersPage from './components/Designers.vue'
+import QuizPage from './components/Quiz.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/about', component: AboutPage },
   { path: '/profile', component: ProfilePage},
   { path: '/group-chats', component: GroupPage},
-  { path: '/designers', component: DesignersPage}
+  { path: '/designers', component: DesignersPage},
+  { path: '/quiz', component: QuizPage}
 ]
 
 const router = createRouter({
