d1c8dcd19e1ab5bdb91fda12c175916532873405
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-12-19 14:51:48 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
-
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api'; 
+const VUE_APP_API_URL = process.env.VUE_APP_API_URL;
+console.log(VUE_APP_API_URL);
 
 export const signup = async (username, email, password) => {
-  const response = await axios.post(`${API_URL}/signup`, { username, email, password });
+  const response = await axios.post(`${VUE_APP_API_URL}/signup`, { username, email, password });
   return response.data;
 };
 
 export const login = async (email, password) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
+  const response = await axios.post(`${VUE_APP_API_URL}/login`, { email, password });
   return response.data;
 };
