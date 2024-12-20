7624a3e1b9574bb41d9bfcdca1720e0e26c26c1e
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-12-20 21:33:41 +0100

    Update Profile.vue

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -320,7 +320,7 @@ export default {
       const token = localStorage.getItem('token');
       try {
         await axios.put(
-          `${VUE_APP_API_URL}/api/profile`, 
+          `${VUE_APP_API_URL}/profile`, 
           { username: this.username }, 
           { headers: { Authorization: `Bearer ${token}` } }
         );
@@ -336,7 +336,7 @@ export default {
     async unlikeImage(likeId) { 
       const token = localStorage.getItem('token');
       try {
-        await axios.delete(`${VUE_APP_API_URL}/api/likes/${likeId}`, {
+        await axios.delete(`${VUE_APP_API_URL}/likes/${likeId}`, {
           headers: { Authorization: `Bearer ${token}` },
         });
         this.likedImages = this.likedImages.filter(image => image.id !== likeId);
@@ -354,7 +354,7 @@ export default {
       try {
         const token = localStorage.getItem('token');
         await axios.put(
-          `${VUE_APP_API_URL}/api/profile/designer`,
+          `${VUE_APP_API_URL}/profile/designer`,
           {},
           { headers: { Authorization: `Bearer ${token}` } }
         );
@@ -390,7 +390,7 @@ export default {
     async deleteHistory(dateTime) {
       const token = localStorage.getItem('token');
       try {
-        await axios.delete(`${VUE_APP_API_URL}/api/history/${dateTime}`, {
+        await axios.delete(`${VUE_APP_API_URL}/history/${dateTime}`, {
           headers: { Authorization: `Bearer ${token}` },
         });
         this.history = this.history.filter(item => item.dateTime !== dateTime);
